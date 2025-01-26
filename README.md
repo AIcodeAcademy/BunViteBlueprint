a67910eca0d98d8d789c6fc57127ef66353b01f4
Alberto Basalo <albertobasalo@hotmail.com>
2025-01-26 12:37:04 +0100

    style: Add custom fonts and update project styling with Pico CSS

--- README.md
+++ README.md
@@ -18,5 +18,24 @@ bun i @picocss/pico
 bun i -D @biomejs/biome
 bun lint
 bun format
+# Install required extensions
+# run dev
 bun dev
 ```
+
+---
+
+<footer>
+  <h3>🧑🏼‍💻 By <a href="https://albertobasalo.dev" target="blank">Alberto Basalo</a> </h3>
+  <p>
+    <a href="https://twitter.com/albertobasalo" target="blank">
+      <img src="https://img.shields.io/twitter/follow/albertobasalo?logo=twitter&style=for-the-badge" alt="twitter albertobasalo" />
+    </a>
+  </p>
+  <p>
+    <a href="https://github.com/albertobasalo" target="blank">
+      <img 
+        src="https://img.shields.io/github/followers/albertobasalo?logo=github&label=profile albertobasalo&style=for-the-badge" alt="git albertobasalo" />
+    </a>
+  </p>
+</footer>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
+
 
 const appElement = document.querySelector<HTMLDivElement>("#app");
 if (!appElement) throw new Error("Could not find #app element");
@@ -9,21 +7,19 @@ const html = String.raw;
 appElement.innerHTML = html`
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <span class="logo">Blueprint</span>
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-     raw
+      <article>
+      <h1>Bun Vite </h1>
+      <p>
+        Run by <a href="https://bun.sh" target="_blank">Bun</a>
     </p>
+    <p>
+      Packed by <a href="https://vitejs.dev" target="_blank">Vite</a>
+    </p>
+    <p>
+        Style by <a href="https://picocss.com" target="_blank">Pico CSS</a>
+      </p>
+    </article>
   </div>
 `;
-
-const counterElement = document.querySelector<HTMLButtonElement>("#counter");
-if (!counterElement) throw new Error("Could not find #counter element");
-setupCounter(counterElement);
